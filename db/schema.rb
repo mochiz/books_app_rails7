cf37a79d34c5c45ee1b82a78dcbb1be372892a61
mochiz <mochi.hiro43@gmail.com>
2024-10-20 21:45:53 +0900

    Bookモデルにpictureカラムを追加して画像アップロードできるようにした

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2024_10_20_121354) do
+ActiveRecord::Schema[7.2].define(version: 2024_10_20_124029) do
   create_table "books", force: :cascade do |t|
     t.string "title"
     t.text "memo"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "author"
+    t.string "picture"
   end
 end
